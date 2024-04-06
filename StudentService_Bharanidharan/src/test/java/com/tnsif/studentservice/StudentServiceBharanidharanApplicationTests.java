e14038ff7cea9f1efeb66a087b44b6364aab83bf
BHARANIDHARAN V <BHARANIDHARAN V@DESKTOP-K771HQ6>
2024-04-06 21:43:30 +0530

    this is my student service project

---
+++ StudentService_Bharanidharan/src/test/java/com/tnsif/studentservice/StudentServiceBharanidharanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tnsif.studentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentServiceBharanidharanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
